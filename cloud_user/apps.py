3f845dc0ee6493ef55aaed22365dec018123b0af
Tryd0g0lik <work80@mail.ru>
2024-12-30 17:07:36 +0700

    [cloud_user]: code  was update

--- cloud_user/apps.py
+++ cloud_user/apps.py
@@ -6,7 +6,7 @@ from cloud_user.contribute.utilites import send_activation_notificcation
 from logs import configure_logging
 configure_logging(logging.INFO)
 log = logging.getLogger(__name__)
-
+log.info(f"{__name__} START")
 
 class CloudUserConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
@@ -17,6 +17,14 @@ class CloudUserConfig(AppConfig):
 user_registerred = Signal(use_caching=False)
 log.info(f"{__name__} Signal WAS STARTED")
 def user_registered_dispatcher(sender, **kwargs)-> bool:
+    """
+    TODO: Send an activation message by the user email.\
+        This is interface from part of registration the new user.\
+        Message, it contains the signature of link for authentication
+    :param sender:
+    :param kwargs:
+    :return: bool
+    """
     __text = f"[{user_registered_dispatcher.__name__}]: "
     log.info(f"{__text} START")
     __resp_bool = False
@@ -36,6 +44,7 @@ def user_registered_dispatcher(sender, **kwargs)-> bool:
         log.info(__text)
         return __resp_bool
         
-
+log.info(f"{__name__} the {user_registerred.__name__} is before the beginning")
 user_registerred.connect(weak=False,
                          receiver=user_registered_dispatcher)
+log.info(f"{__name__} END")
