611661d09230698296e4437cf0638467145fcb83
Tryd0g0lik <work80@mail.ru>
2025-02-12 20:12:05 +0700

    Description was addede for the Csrft()

--- project/__init__.py
+++ project/__init__.py
@@ -2,7 +2,18 @@ from django.http import JsonResponse
 
 
 state = ''
+
 class Csrft():
+    """
+    Class Csrft() it is analogous for the 'useState()' from react.
+    Here is a task  - create the 'csrftoken' and compare the this 'csrftoken' with \
+    'csrftoken' from the request.
+    First, we need to get the 'csrftoken' from the cenerator.
+    Then we will be compare the 'csrftoken' (backend's version) with \
+     the 'csrftoken' (vertion of client) from the request.
+    :param state: the state of`'csrftoken'.
+    :param set_state: For a set the `'csrftoken' from  csrf-token's generator.
+    """
     # pass
     def __init__(self):
         self.state = ''
